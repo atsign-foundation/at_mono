87b3e7eccaed0484c0ab07f61709b9ec741d9041
xavierchanth <xchanthavong@gmail.com>
2022-10-24 16:12:54 -0400

    chore: Final formatting and documentation

--- tools/package_diagrams.py
+++ tools/package_diagrams.py
@@ -22,6 +22,7 @@ def remove_subgraphs(gviz:str) -> str:
   return gviz
 
 def write_svg(gviz:str, path:str) -> None:
+  # Write the graphviz dot notation to the file at the given path
   graphs = graph_from_dot_data(gviz)
   svg = graphs[0].create(prog='dot', format='svg').decode('ascii')
   svg = autoscale_svg(svg)
@@ -30,18 +31,21 @@ def write_svg(gviz:str, path:str) -> None:
     f.close()
 
 def autoscale_svg(svg:str) -> str:
+  # This method updates the CSS scaling applied to the diagram
+  # Uses regex to pull the viewBox and canvas width and recalculates what the
+  # correct scaling should be, finally it applies that scaling to the SVG.
   scale_regex = r'scale\([\d\.]* [\d\.]*\)'
 
   vb_regex = r'viewBox="[\d\.]* [\d\.]* ([\d\.]*) ([\d\.]*)"'
   bg_regex = r'<polygon fill="white" stroke="transparent" points="([\-\d\.]*),([\-\d\.]*) [\-\d\.]*,[\-\d\.]* ([\-\d\.]*),([\-\d\.]*) [\-\d\.]*,[\-\d\.]* [\-\d\.]*,[\-\d\.]*"\/>'
 
-  vb = findall(vb_regex, svg)[0]
-  bg = findall(bg_regex, svg)[0]
+  vb = findall(vb_regex, svg)[0] # Viewbox (width, height)
+  bg = findall(bg_regex, svg)[0] # Coordinates of the canvas (left X, top Y, right X, bottom Y)
 
-  scaled_width = float(bg[2])-float(bg[0])
-  scaled_height = float(bg[3])-float(bg[1])
+  canvas_width = float(bg[2])-float(bg[0]) # Calculate canvas width
+  canvas_height = float(bg[3])-float(bg[1]) # Calculate canvas width
 
-  scale = min(abs(float(vb[0])/scaled_width), abs(float(vb[1])/scaled_height))
+  scale = min(abs(float(vb[0])/canvas_width), abs(float(vb[1])/canvas_height)) # Compute both scales and use the min as the true scale
 
   return sub(scale_regex, 'scale({0} {1})'.format(scale, scale), svg)
 
